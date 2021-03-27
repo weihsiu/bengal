2c2f1e19feb5c967e87fe6239f3644fe5f7369a1
Walter Chang <weihsiu@gmail.com>
2021-03-28 02:21:25 +0800

    functor deriving in progress

--- src/main/scala/bengal/Functor.scala
+++ src/main/scala/bengal/Functor.scala
@@ -1,7 +1,23 @@
 package bengal
 
+import scala.compiletime.{erasedValue, summonAll}
+import scala.deriving.*
+
 trait Functor[F[_]]:
   extension [A](x: F[A])
     def map[B](f: A => B): F[B]
 
-object Functor extends TCApply[Functor]
+object Functor extends TCApply[Functor]:
+  def iterator[T](p: T) = p.asInstanceOf[Product].productIterator
+
+  def functorSum[T[_]](s: Mirror.SumOf[T[_]], elems: => List[Functor[_]]): Functor[T] =
+    ???
+
+  def functorProduct[T[_]](p: Mirror.ProductOf[T[_]], elems: => List[Functor[_]]): Functor[T] =
+    ???
+
+  inline given derived[T[_]](using m: Mirror.Of[T[_]]): Functor[T] =
+    lazy val elemInstances = summonAll[m.MirroredElemTypes].toList.map(_.asInstanceOf[Functor[_]])
+    inline m match
+      case s: Mirror.SumOf[T[_]] => functorSum(s, elemInstances)
+      case p: Mirror.ProductOf[T[_]] => functorProduct(p, elemInstances)
