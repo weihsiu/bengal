d1d5bb3660c14656beaf46d2b2221c5d267a3a3b
Walter Chang <weihsiu@gmail.com>
2021-03-29 01:48:55 +0800

    functor in progress

--- src/main/scala/bengal/Functor.scala
+++ src/main/scala/bengal/Functor.scala
@@ -10,14 +10,30 @@ trait Functor[F[_]]:
 object Functor extends TCApply[Functor]:
   def iterator[T](p: T) = p.asInstanceOf[Product].productIterator
 
-  def functorSum[T[_]](s: Mirror.SumOf[T[_]], elems: => List[Functor[_]]): Functor[T] =
+  def functorSum[T[_], A](s: Mirror.SumOf[T[A]], elems: => List[Functor[_]]): Functor[T] =
     ???
 
-  def functorProduct[T[_]](p: Mirror.ProductOf[T[_]], elems: => List[Functor[_]]): Functor[T] =
-    ???
+  def functorProduct[T[_], A](p: Mirror.ProductOf[T[A]], elems: => List[Functor[_]]): Functor[T] =
+    if elems.isEmpty then
+      new Functor[T]:
+        extension [A](x: T[A])
+          def map[B](f: A => B): T[B] = x.asInstanceOf[T[B]]
+    else
+      new Functor[T]:
+        extension [A](x: T[A])
+          def map[B](f: A => B): T[B] = p.fromProduct(new Product:
+            val productArity = 1
+            def productElement(n: Int): Any =
+              elems.head.map(x)(f)
+          )
 
-  inline given derived[T[_]](using m: Mirror.Of[T[_]]): Functor[T] =
+  inline given derived[T[_], A](using m: Mirror.Of[T[A]]): Functor[T] =
     lazy val elemInstances = summonAll[m.MirroredElemTypes].toList.map(_.asInstanceOf[Functor[_]])
     inline m match
       case s: Mirror.SumOf[T[_]] => functorSum(s, elemInstances)
       case p: Mirror.ProductOf[T[_]] => functorProduct(p, elemInstances)
+
+@main
+def functorDerivesTest() =
+  case object CO derives Functor
+  println(CO.map(identity))
